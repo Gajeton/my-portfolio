10c38336562f674ccb61688d9416b2850c415c29
Gajeton <antoine.familiar@gmail.com>
2023-08-03 20:47:46 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-	reactStrictMode: true,
-	swcMinify: true,
-	trailingSlash: true,
-}
+    /* config options here */
+    trailingSlash: true,
+  }
+   
   module.exports = nextConfig
