7b879981694bc086e2eef7fed1dc831477e8bca7
Gajeton <antoine.familiar@gmail.com>
2023-11-22 14:24:37 +1300

    update

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/my-portofolio",
+  basePath: "/my-portoflio",
 
   /**
    * Disable server-based image optimization. Next.js does not support
